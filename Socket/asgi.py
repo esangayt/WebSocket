cd23dee5bd0ec332cb2b3e2b72518ba9ea47c0a9
esangayt <24_de_7@protonmail.com>
2024-08-09 01:53:04 -0500

    Customers channels

--- Socket/asgi.py
+++ Socket/asgi.py
@@ -11,14 +11,18 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from .routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chat.routing import websocket_urlpatterns
+
+# from .routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Socket.settings')
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    ),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+    )
 })
